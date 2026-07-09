4b66ea447f416924eacdccafd14cb64c032b069d
bhargav-Q <bhargav.raj@quantana.com.au>
2026-07-09 16:02:48 +0530

    Extrcted the course cards with for loop

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ with open('website.html', 'r') as html_file:
 
     #by using beatifulsoup we are apply lxml format to the content  
     soup=BeautifulSoup(content, 'lxml') # print(soup)
-
-    # find() searches and gives the first tag
-    # find_all() searches and gives all the tags in a
-    # # tags=soup.find('h2') here we can only insert the tag names to get some output
-    tags=soup.find_all('h3') # print(tags)
-
-    # using this for loop we are only displaying the text inside the tags in each line
-    for t in tags:
-        print(t.text,end=" ")
+    course_cards=soup.find_all('div', class_="card mb-3")
+    for course in course_cards:
+       print(course.h3)
