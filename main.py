082b2607f08b0277364beb021f0d4811e325e0d8
bhargav-Q <bhargav.raj@quantana.com.au>
2026-07-09 15:14:40 +0530

    Understood about find() and find_all()

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
 
 with open('website.html', 'r') as html_file:
+
+    # read() shows the exact code
     content = html_file.read()
-    print(content)
+
+    #by using beatifulsoup we are apply lxml format to the content  
+    soup=BeautifulSoup(content, 'lxml')
+
+    # find() searches and gives the first tag
+    # find_all() searches and gives all the tags in a
+    # #tags=soup.find('h2') here we can only insert the tag names to get some output
+    tags=soup.find_all('h2')
+    print(tags)
