a7e22d297b2d8c99ec847170232d98baa6e8d291
bhargav-Q <bhargav.raj@quantana.com.au>
2026-07-13 14:46:27 +0530

    phase-3 : successfully built an engine that takes a raw HTML string and converts it into structured, analytical data (a dictionary of counts)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from bs4 import BeautifulSoup
 import requests
+
 def get_url():
-    url=input("Enter the URL: ").strip()
+    url = input("Enter the URL: ").strip()
     if(url.startswith("https://")): # Consider handling both http:// and https:// URLs in a future revision.
         return url
     return "https://"+url 
@@ -16,16 +18,34 @@ def fetch_page(url):
 def display_response_info(response):
     print(response.status_code)
     print(len(response.content))
-    print(response.headers)
+    # print(response.headers)
+
+def analyse_html(html_text):
+    soup = BeautifulSoup(html_text, 'lxml')
+    
+    # Use len() around your find_all() calls to get the counts!
+    report = {
+        "title": soup.title.text if soup.title else "No Title",
+        "h1": len(soup.find_all('h1')),
+        "h2": len(soup.find_all('h2')),
+        "h3": len(soup.find_all('h3')),
+        "links": len(soup.find_all('a')),
+        "images": len(soup.find_all('img')),
+        "tables": len(soup.find_all('table')),
+        "forms": len(soup.find_all('form'))
+    }
+    
+    return report
 
 def main():
     url = get_url()
     response = fetch_page(url)
     if response:
         display_response_info(response)
+        report = analyse_html(response.text)
+        print(report) 
     else:
         print("Failed to fetch page")
 
 if __name__ == "__main__":
     main()
-    
